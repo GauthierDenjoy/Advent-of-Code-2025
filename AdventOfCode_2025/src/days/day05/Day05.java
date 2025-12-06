e731386d0ff9e8d6ccf671d47e5c9589c47327dc
AstralAppaloosa <astralappaloosa@gmail.com>
2025-12-06 21:14:37 +0100

    haha i ll do it when i get my credentials back

---
+++ AdventOfCode_2025/src/days/day05/Day05.java
@@ -0,0 +1,34 @@
+package days.day05;
+
+import days.DayChallenge;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class Day05 extends DayChallenge {
+
+    public Day05() {
+        super(5);
+    }
+
+    @Override
+    protected void runPartOne() {
+
+
+    }
+
+    @Override
+    protected void runPartTwo() {
+
+    }
+
+
+
+    private List<Long> readValuesConverted() {
+        List<Long> values = new ArrayList<>();
+        List<String> lines = readInputFile();
+
+
+        return values;
+    }
+}
---
+++ AdventOfCode_2025/src/days/day06/Day06.java
@@ -0,0 +1,35 @@
+package days.day06;
+
+import days.DayChallenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class Day06 extends DayChallenge {
+
+    public Day06() {
+        super(6);
+    }
+
+    @Override
+    protected void runPartOne() {
+
+
+    }
+
+    @Override
+    protected void runPartTwo() {
+
+    }
+
+
+
+    private List<Long> readValuesConverted() {
+        List<Long> values = new ArrayList<>();
+        List<String> lines = readInputFile();
+
+
+        return values;
+    }
+}
